24cee5e5cb5e9e12a0e770e4af18073782dc22da
Jeff Zeng <zhh13950510580@outlook.com>
2023-08-12 17:26:12 +0800

    [Feat] [WIP] Build NeteasePlaylistViewModel

--- src/HyPlayer.App/ViewModels/NeteasePlaylistViewModel.cs
+++ src/HyPlayer.App/ViewModels/NeteasePlaylistViewModel.cs
@@ -1,22 +1,38 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using HyPlayer.App.Interfaces.ViewModels;
 using HyPlayer.NeteaseProvider.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace HyPlayer.App.ViewModels;
 
-public class NeteasePlaylistViewModel : ObservableObject, IScrollableViewModel, IConnectedViewModel, IScopedViewModel
+public partial class NeteasePlaylistViewModel : ObservableObject, IScrollableViewModel, IConnectedViewModel, IScopedViewModel
 {
-    public double? ScrollValue { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-    public int? ConnectedItemIndex { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-    public string? ConnectedElementName { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public double? ScrollValue { get; set ; }
+    public int? ConnectedItemIndex { get ; set; }
+    public string? ConnectedElementName { get; set; }
 
     private readonly NeteaseProvider.NeteaseProvider _provider;
-    private readonly NeteasePlaylist _playList;
-    
+    private readonly NeteasePlaylist? _playList;
+
+    public string Title => _playList.Name;
+    public string Description => _playList.Description;
+    public long PlayCount => _playList.PlayCount;
+    public long ShareCount => _playList.ShareCount;
+
+    public List<NeteaseSong>? SongsList;
+
 
     public NeteasePlaylistViewModel(NeteaseProvider.NeteaseProvider provider, NeteasePlaylist playList = null)
     {
         _provider = provider;
         _playList = playList;
+    }
+
+    [RelayCommand]
+    public async Task GetSongsAsync()
+    {
+        //SongsList = await _playList.GetAllItemsAsync();
     }
 }
