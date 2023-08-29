823dc2236eb06a93bba53439b3469f983b721883
Jeff Zeng <zhh13950510580@outlook.com>
2023-08-29 19:44:43 +0800

    [WIP] 用户页构建

--- src/HyPlayer.App/ViewModels/CurrentPlayingViewModel.cs
+++ src/HyPlayer.App/ViewModels/CurrentPlayingViewModel.cs
@@ -8,7 +8,7 @@ using HyPlayer.NeteaseProvider.Models;
 using HyPlayer.PlayCore.Abstraction;
 using HyPlayer.PlayCore.Abstraction.Models.Notifications;
 using HyPlayer.PlayCore.Abstraction.Models.SingleItems;
-using Depository.Abstraction.Interfaces.NotificationHub;
+using Depository.Abstraction.Interfaces;
 
 namespace HyPlayer.ViewModels;
 
--- src/HyPlayer.App/Views/Pages/UserPage.xaml.cs
+++ src/HyPlayer.App/Views/Pages/UserPage.xaml.cs
@@ -1,20 +1,39 @@
-﻿using HyPlayer.Interfaces.Views;
-using HyPlayer.ViewModels;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using HyPlayer.Interfaces.Views;
+using HyPlayer.ViewModels;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace HyPlayer.Views.Pages
 {
-    public partial class UserPage : Page
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class UserPage : UserPageBase
     {
         public UserPage()
         {
-            this.InitializeComponent();
+            this.InitializeComponent();   
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            if(e.Parameter !=)
         }
     }
 
