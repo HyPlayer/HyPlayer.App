05b145243adf051db07c7189effe2c483ad5776b
maxinglo <90967786+maxinglo@users.noreply.github.com>
2024-05-03 04:52:31 +0800

    添加需要的转换器

---
+++ src/HyPlayer.App/ViewModels/Converters/ArtistListToStringConverter.cs
@@ -0,0 +1,30 @@
+using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HyPlayer.NeteaseProvider.Models;
+using Microsoft.UI.Xaml.Data;
+using HyPlayer.PlayCore.Abstraction.Models.Containers;
+
+namespace HyPlayer.ViewModels.Converters
+{
+    internal class ArtistListToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is List<PersonBase> artistList)
+            {
+                return string.Join("、", artistList.Select(t => t.Name));
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
