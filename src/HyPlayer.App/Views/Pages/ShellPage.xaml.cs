e64c7b5a9791032b12bc628fd829b09b0c93708f
Jeff Zeng <zhh13950510580@outlook.com>
2024-07-14 21:31:12 +0800

    [Feat] 移除DesktopNavigationView。

--- src/HyPlayer.App/Views/Pages/ShellPage.xaml.cs
+++ src/HyPlayer.App/Views/Pages/ShellPage.xaml.cs
@@ -1,18 +1,44 @@
+using HyPlayer.Interfaces.Views;
+using HyPlayer.ViewModels;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace HyPlayer.Views.Pages
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ShellPage : Page
+    public sealed partial class ShellPage : ShellPageBase
     {
         public ShellPage()
         {
             this.InitializeComponent();
+            App.contentFrame = contentFrame;
+        }
+
+        private void NavView_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
+        {
+            var invokedItem = (args.InvokedItemContainer as NavigationViewItem)?.Tag?.ToString();
+            if (invokedItem == null) return;
+            var invokedItemTag = HyPlayer.App.GetService<IPageService>().GetPageType(invokedItem);
+            var invokedItemName = (args.InvokedItemContainer as NavigationViewItem)?.Content?.ToString();
+
+
+            HyPlayer.App.GetService<INavigationService>().NavigateTo(invokedItemTag);
+            // NavView.Header = invokedItemName;        
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            NavView.SelectedItem = Home;
+        }
+    }
+
+    public class ShellPageBase : AppPageWithScopedViewModelBase<ShellViewModel>
+    {
+        public ShellPageBase()
+        {
         }
     }
 }
