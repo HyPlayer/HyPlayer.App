31d43f72c561ee9f084de73ad9a0f8965a4dcc0a
Jeff Zeng <zhh13950510580@outlook.com>
2024-07-15 22:34:52 +0800

    [Refactor] 重构PageService。

--- src/HyPlayer.App/Services/PageService.cs
+++ src/HyPlayer.App/Services/PageService.cs
@@ -1,18 +1,55 @@
-﻿using HyPlayer.Interfaces.Services;
+using HyPlayer.Interfaces.Services;
 using HyPlayer.Views.Pages;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.UI.Xaml.Controls;
 
 namespace HyPlayer.Services
 {
-    class PageService : IPageService
+    public class PageService : IPageService
     {
+        private readonly Dictionary<string, Type> _pages = new();
+
+        public PageService()
+        {
+            Configure<HomePage>();
+        }
+
         public Type GetPageType(string key)
         {
-            if (key == "HomePage")
+            Type? pageType;
+            lock (_pages)
             {
-                return typeof(HomePage);
+                if (!_pages.TryGetValue(key, out pageType))
+                {
+                    throw new ArgumentException($"Page not found: {key}. Did you forget to call PageService.Configure?");
+                }
             }
-            else return typeof(ErrorPage);
+
+            return pageType;
+        }
+
+        private void Configure<V>()
+            where V : Page
+        {
+            lock (_pages)
+            {
+                var t = typeof(V);
+                var key = t.Name;
+                if (_pages.ContainsKey(key))
+                {
+                    throw new ArgumentException($"The key {key} is already configured in PageService");
+                }
+
+                var type = typeof(V);
+                if (_pages.ContainsValue(type))
+                {
+                    throw new ArgumentException($"This type is already configured with key {_pages.First(p => p.Value == type).Key}");
+                }
+
+                _pages.Add(key, type);
+            }
         }
     }
 }
