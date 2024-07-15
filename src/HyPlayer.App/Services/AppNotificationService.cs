d9a3d1e86c6efecf9505301d2946b97743319a0a
Jeff Zeng <zhh13950510580@outlook.com>
2024-07-15 23:25:10 +0800

    [Feat]》 添加AppNotificationService。

---
+++ src/HyPlayer.App/Services/AppNotificationService.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Specialized;
+using System.Web;
+
+using HyPlayer.Interfaces.Services;
+
+using Microsoft.Windows.AppNotifications;
+
+namespace HyPlayer.Services;
+
+public class AppNotificationService : IAppNotificationService
+{
+    public AppNotificationService()
+    {    
+    }
+
+    ~AppNotificationService()
+    {
+        Unregister();
+    }
+
+    public void Initialize()
+    {
+        AppNotificationManager.Default.NotificationInvoked += OnNotificationInvoked;
+
+        AppNotificationManager.Default.Register();
+    }
+
+    public void OnNotificationInvoked(AppNotificationManager sender, AppNotificationActivatedEventArgs args)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Show(string payload)
+    {
+        var appNotification = new AppNotification(payload);
+
+        AppNotificationManager.Default.Show(appNotification);
+
+        return appNotification.Id != 0;
+    }
+
+    public NameValueCollection ParseArguments(string arguments)
+    {
+        return HttpUtility.ParseQueryString(arguments);
+    }
+
+    public void Unregister()
+    {
+        AppNotificationManager.Default.Unregister();
+    }
+}
